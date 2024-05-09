e168a992e402c3d9e717dc1249036aca767bdb86
Ferris <ferris.evans@outlook.com>
2024-05-09 14:59:41 +0800

    import math

---
+++ book3/ch01/bk3_ch01_01.py
@@ -0,0 +1,10 @@
+import math
+
+# Print the value of pi
+print ('pi = ', math.pi)
+
+# Print the value of e
+print ('e = ', math.e)
+
+# Print the value of square root of 2
+print ('sqrt(2) = ', math.sqrt(2))
