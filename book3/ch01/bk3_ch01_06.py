b3841cd6309d06f9efd8398b283dab5a5ee294ca
Ferris <ferris.evans@outlook.com>
2024-05-19 21:50:23 +0800

    ch01

--- book3/ch01/bk3_ch01_06.py
+++ book3/ch01/bk3_ch01_06.py
@@ -2,14 +2,13 @@ import numpy as np
 
 A = [[1, 2, 3],
      [4, 5, 6]]
-
 B = [[1, 0, 0],
      [0, 1, 0]]
 
 A_plus_B = [
     [A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))
 ]
-
 print(A_plus_B)
+
 print(np.array(A) + np.array(B))
 print(np.add(A, B))
