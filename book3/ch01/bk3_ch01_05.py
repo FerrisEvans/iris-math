8c0e59ce2ae2a8751efe3bf926d4ba74b3c115c0
Ferris <ferris.evans@outlook.com>
2024-05-19 21:41:48 +0800

    ch01

---
+++ book3/ch01/bk3_ch01_05.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print([x + y for x, y in zip(list1, list2)])
+
+print(list(map(lambda x, y: x + y, list1, list2)))
+
+x = np.array(list1)
+y = np.array(list2)
+print(x + y)
+
+print(np.add(list1, list2))
