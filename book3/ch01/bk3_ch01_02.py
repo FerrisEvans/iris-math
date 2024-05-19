6cd0ef1679aea4e22e081a1c026291111972e2f7
Ferris <ferris.evans@outlook.com>
2024-05-19 21:35:51 +0800

    ch02

---
+++ book3/ch01/bk3_ch01_02.py
@@ -0,0 +1,11 @@
+num = float(input("Enter a number: "))
+
+if num.is_integer():
+
+    if (num % 2) == 0:
+       print("{0} is even ".format(int(num)))
+    else:
+       print("{0} is odd ".format(int(num)))
+
+else:
+    print("{0} is not an integer ".format(num))
