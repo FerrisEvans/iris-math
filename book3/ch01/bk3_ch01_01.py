37e290a0297dac35c89c798bfa12fd1f43b442cd
Ferris <ferris.evans@outlook.com>
2024-05-19 21:35:46 +0800

    ch01

--- book3/ch01/bk3_ch01_01.py
+++ book3/ch01/bk3_ch01_01.py
@@ -1,4 +1,5 @@
 import math
+from mpmath import mp
 
 # Print the value of pi
 print ('pi = ', math.pi)
@@ -8,3 +9,14 @@ print ('e = ', math.e)
 
 # Print the value of square root of 2
 print ('sqrt(2) = ', math.sqrt(2))
+
+mp.dps = 1000 + 1
+
+print('print 1000 digits of pi behind decimal point')
+print(mp.pi)
+
+print('print 1000 digits of e behind decimal point')
+print(mp.e)
+
+print('print 1000 digits of sqrt(2) behind decimal point')
+print(mp.sqrt(2))
