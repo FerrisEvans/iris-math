79da53ad74e1a058e96c0b06f38d42d8753112fe
Ferris <ferris.evans@outlook.com>
2024-05-21 23:25:52 +0800

    ch03

---
+++ book3/ch03/bk3_ch03_03.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_start = 6
+n_stop = 50
+n_array = np.linspace(n_start, n_stop, n_stop - n_start + 1)
+
+pi_lower_b = np.sin(np.pi / n_array) * n_array
+pi_upper_b = np.tan(np.pi / n_array) * n_array
+
+fig, ax = plt.subplots()
+
+plt.axhline(y=np.pi, color='r', linestyle='-')
+plt.plot(n_array, pi_lower_b, color='b')
+plt.plot(n_array, pi_upper_b, color='g')
+plt.fill_between(n_array, pi_lower_b, pi_upper_b, color='#DEEAF6')
+plt.tight_layout()
+plt.xlabel('Number of sides, n')
+plt.ylabel('Estimate of $\pi$')
