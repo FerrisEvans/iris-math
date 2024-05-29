69353cf4c026536358083e49fb0d505fbbb76e39
Ferris <ferris.evans@outlook.com>
2024-05-29 23:05:58 +0800

    ch13

---
+++ book3/ch11/bk3_ch11_03.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-2, 2, 100);
+
+
+def plot_curve(x, y):
+    fig, ax = plt.subplots()
+
+    plt.xlabel("$\it{x}$")
+    plt.ylabel("$\it{f}(\it{x})$")
+    plt.plot(x, y, linewidth=1.5)
+    plt.axhline(y=0, color='k', linewidth=1.5)
+    plt.axvline(x=0, color='k', linewidth=1.5)
+    ax.grid(linestyle='--', linewidth=0.25, color=[0.5, 0.5, 0.5])
+    plt.axis('equal')
+    plt.xticks(np.arange(-2, 2.5, step=0.5))
+    plt.yticks(np.arange(y.min(), y.max() + 0.5, step=0.5))
+    ax.set_xlim(x.min(), x.max())
+    ax.set_ylim(y.min(), y.max())
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    plt.axis('square')
+
+
+# %% plot linear, quadratic, and cubic functions
+
+plt.close('all')
+
+# linear function
+y = x + 1
+plot_curve(x, y)
+
+# linear function
+y = -x + 1
+plot_curve(x, y)
+
+# quadratic function, parabola opens upwards
+# y = np.power(x,2) - 2;
+y = x ** 2 - 2
+plot_curve(x, y)
+
+# quadratic function, parabola opens downwards
+# y = -np.power(x,2) + 2;
+y = -x ** 2 + 2
+plot_curve(x, y)
+
+# cubic function
+# y = np.power(x,3) - x;
+y = x ** 3 - x
+plot_curve(x, y)
+
+# cubic function
+# y = -np.power(x,3) + x;
+y = -x ** 3 + x
+plot_curve(x, y)
